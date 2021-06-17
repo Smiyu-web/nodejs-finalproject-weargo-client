f0daf1a0e9392b7d7e46a6f57eb997f06c01d5d9
Miu <miyuuu0728@gmail.com>
2021-06-16 22:28:12 -0700

    changed btn

--- src/components/navbar/user/Signup.jsx
+++ src/components/navbar/user/Signup.jsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../../ui/Button";
 import InputWrapper from "../../ui/InputWrapper";
 
 const Signup = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [confirmPassword, setConfirmPassword] = useState();
+
   return (
     <div className="pt-28 flex flex-col items-center">
       <h2 className="py-6">Sign Up</h2>
       <div>
-        <InputWrapper title="Name" type="name" name="name" />
-        <InputWrapper title="Email" type="email" name="email" />
-        <InputWrapper title="Password" type="password" name="password" />
-        <InputWrapper
-          title="Confirm Password"
-          type="password"
-          name="confirm password"
-        />
-      </div>
-      <div>
-        <Button button="Sign up" />
+        <form onSubmit={submit}>
+          <InputWrapper
+            title="Name"
+            type="text"
+            name="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <InputWrapper
+            title="Email"
+            type="email"
+            name="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <InputWrapper
+            title="Password"
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <InputWrapper
+            title="Confirm Password"
+            type="password"
+            name="confirm password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </form>
+        <div className="text-center">
+          <input className="input_btn" type="button" value="Sign up" />
+        </div>
       </div>
       <div className="text-center">
         <p>Do you already have an account?</p>
