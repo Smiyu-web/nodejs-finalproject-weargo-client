c49e15af2700e68e63c19e484621083a1b2dbb3f
Miu <miyuuu0728@gmail.com>
2021-06-30 10:06:03 -0700

    add

--- src/components/home/ListByTag.jsx
+++ src/components/home/ListByTag.jsx
@@ -9,27 +9,29 @@ const ListByTag = () => {
   useEffect(async () => {
     const result = await Axios("http://localhost:4000/style/");
     setData(result.data);
-    console.log(result);
+    console.log("data: " + result);
   }, []);
+
+  const styleList = data.map((result) => {
+    return (
+      <>
+        <p>{result.title}</p>
+        <ImgWrapper
+          className="shadow-lg"
+          src={result.image}
+          alt="outfit"
+          user={result.userId}
+        />
+      </>
+    );
+  });
 
   return (
     <div className="mt-8 mx-9">
       <h6 className="mb-2 font-thin">#spring</h6>
       <div className="flex justify-center">
         <div className="grid grid-flow-col grid-cols-3 gap-4 text-center">
-          {data.map((result) => {
-            return (
-              <>
-                <p>{result.title}</p>
-                <ImgWrapper
-                  className="shadow-lg"
-                  src={result.image}
-                  alt="outfit"
-                  user={result.userId}
-                />
-              </>
-            );
-          })}
+          {styleList}
         </div>
       </div>
     </div>
